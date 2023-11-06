41faedb04f8ef5e018a09a642be50b639628ff57
Icaro <146675789+IcaroRubem@users.noreply.github.com>
2023-11-06 14:56:18 -0300

    update script/rotate-cartesian.js

--- script/rotate-cartesian.js
+++ script/rotate-cartesian.js
@@ -1,7 +1,22 @@
 const cartesian = document.querySelector('.cartesian')
+let moveX = 0
+let moveY = 0
 
-document.addEventListener('touchmove', (touch) => {
-    const {clientX, clientY} = touch.touches[0]
+document.addEventListener('touchstart', (touch) => {
+    const {
+        clientX, clientY
+    } = touch.touches[0]
+    moveX = clientX
+    moveY = clientY
+})
     
+document.addEventListener('touchmove', (touch) => {
+    const {
+        clientX, clientY
+    } = touch.touches[0]
+    
+    if (clientX > moveX) {
+        
+    }
     console.log(clientX, clientY)
 })
